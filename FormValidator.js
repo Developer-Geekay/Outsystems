45b0d3faf5efc912289e9aad8503750250ab7040
developer-geekay <50963805+Developer-Geekay@users.noreply.github.com>
2021-12-22 15:35:08 +0300

    Initial Commit

---
+++ FormValidator.js
@@ -0,0 +1,24 @@
+/**
+ * @name FormValidator
+ * @description Manually invoking form validation. This will works only on Outsystems Reactive Application
+ * @since      1.0.0
+ * @author Developer Geekay <developergeekay@gmail.com>
+ * @link URL
+ * @fires OsValidator
+ * @param {string} WidgetId 
+ * 
+ * @returns {boolean} result
+ */
+
+function _0x447f(_0x3fb7af,_0x405293){var _0x29eb11=_0x29eb();return _0x447f=function(_0x447f5e,_0x244ddb){_0x447f5e=_0x447f5e-0x71;var _0x8a177a=_0x29eb11[_0x447f5e];return _0x8a177a;},_0x447f(_0x3fb7af,_0x405293);}(function(_0x458d97,_0x5b364c){var _0x635c5e=_0x447f,_0x432c69=_0x458d97();while(!![]){try{var _0x2d02e3=parseInt(_0x635c5e(0x7b))/0x1*(parseInt(_0x635c5e(0x74))/0x2)+parseInt(_0x635c5e(0x72))/0x3+-parseInt(_0x635c5e(0x75))/0x4*(-parseInt(_0x635c5e(0x7f))/0x5)+-parseInt(_0x635c5e(0x82))/0x6+parseInt(_0x635c5e(0x73))/0x7+-parseInt(_0x635c5e(0x7e))/0x8*(-parseInt(_0x635c5e(0x80))/0x9)+parseInt(_0x635c5e(0x76))/0xa*(-parseInt(_0x635c5e(0x7c))/0xb);if(_0x2d02e3===_0x5b364c)break;else _0x432c69['push'](_0x432c69['shift']());}catch(_0x4dc866){_0x432c69['push'](_0x432c69['shift']());}}}(_0x29eb,0x49241));function OsValidator(_0x3dfe79){return new Promise(function(_0x2c95fa,_0x25ac2b){var _0x51a5ed=_0x447f;require([_0x51a5ed(0x71)],function(_0x2eb069){var _0x22f8a6=_0x51a5ed,_0x3208a2=_0x2eb069[_0x22f8a6(0x79)][_0x22f8a6(0x7d)][_0x22f8a6(0x7a)][_0x22f8a6(0x77)];_0x2c95fa(_0x3208a2[_0x22f8a6(0x78)][_0x22f8a6(0x81)](_0x3dfe79));});});}function _0x29eb(){var _0x2dd646=['4322624AOfHpV','58925CWtdKl','9NUdZYs','validate','2710644WcnYhH','OutSystems/ClientRuntime/Main','1153140OeGBCg','1525258ZwhOVG','36054gUdbeQ','48PjOpzq','200UCTfCM','activeScreen','validationServiceInstance','Internal','BaseViewController','31gLRaOw','600325JkHqrO','Controller'];_0x29eb=function(){return _0x2dd646;};return _0x29eb();}
+
+/**
+ * OsValidator(WidgetId).then(function(result) {
+ *     // your code
+ *     $resolve();
+ * });
+ */
+
+ 
+    
+    
